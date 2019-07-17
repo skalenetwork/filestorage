45b43e982f035bd9dd7510face53d8df7fbb241f
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2019-07-17 16:56:24 +0300

    SKALE-1232 Upgrade startUpload unit test

--- test/FileStorage.test.js
+++ test/FileStorage.test.js
@@ -6,19 +6,36 @@ const assert = chai.assert;
 chai.should();
 chai.use(require('chai-as-promised'));
 
+let randomstring = require('randomstring');
 const FileStorage = artifacts.require("./FileStorage");
 
-contract("Filestorage", accounts => {
+contract('Filestorage', accounts => {
     let filestorage;
-    beforeEach(async function () {
-        filestorage = await FileStorage.new({from: accounts[0]});
-    });
 
-    // it('should startUploading', async function () {
-    //     await filestorage.startUpload("aaa.txt", 10, {from: accounts[0]});
-    //     await filestorage.getFileStatus.call(accounts[0]+'/'+"aaaaa.txt")
-    //         .should.eventually
-    //         .rejectedWith('EVM revert instruction without description message');
-    //     // assert.equal(status, 1);
-    // });
+    function ensureStartsWith0x(str) {
+        if (str.length < 2) {return false;}
+        return (str[0] === '0' && str[1] === 'x');
+    }
+
+    function rmBytesSymbol(str){
+        if (!ensureStartsWith0x(str)) return str;
+        return str.slice(2);
+    }
+
+    describe('startUpload', function () {
+        beforeEach(async function () {
+            filestorage = await FileStorage.new({from: accounts[0]});
+        });
+
+        it('should startUploading', async function () {
+            let fileName = randomstring.generate();
+            let fileSize = Math.floor(Math.random()*100);
+            await filestorage.startUpload(fileName, fileSize, {from: accounts[0]});
+            let storagePath = rmBytesSymbol(accounts[0])+'/'+fileName;
+            let status = await filestorage.getFileStatus.call(storagePath);
+            let size = await filestorage.getFileSize.call(storagePath);
+            assert.equal(status, 1, 'Status is incorrect');
+            assert.equal(size, fileSize, "Size is incorrect")
+        });
+    })
 });
