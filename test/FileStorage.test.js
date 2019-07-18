44ec1e39f2585fc497a16b84e1266bf73b5bacac
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2019-07-18 15:54:08 +0300

    SKALE-1232 Add free space and deleteFile unit tests

--- test/FileStorage.test.js
+++ test/FileStorage.test.js
@@ -67,5 +67,58 @@ contract('Filestorage', accounts => {
                 assert.equal(error['receipt']['revertReason'], "Filename should be <= 256 and not contains '/'");
             }
         });
-    })
+
+        describe('Free space limit', function(){
+            let fileNames;
+            let fileCount;
+            before(function(){
+                fileSize = 10 ** 8 - 1;
+                fileNames = [];
+                fileCount = 10;
+            });
+
+            it('should fail when storage is full', async function () {
+                let i = 0;
+                while(i < fileCount){
+                    fileName = randomstring.generate();
+                    fileNames.push(fileName);
+                    await filestorage.startUpload(fileName, fileSize, {from: accounts[0]});
+                    ++i;
+                }
+                try{
+                    fileName = randomstring.generate();
+                    await filestorage.startUpload(fileName, fileSize, {from: accounts[0]});
+                    fileNames.push(fileName);
+                } catch (error) {
+                    assert.equal(error['receipt']['revertReason'], "Not enough free space in the Filestorage");
+                }
+                assert.fail('File was unexpectfully uploaded');
+            });
+
+            afterEach(async function(){
+                for (let j = 0; j < fileNames.length; ++j){
+                    await filestorage.deleteFile(fileNames[j], {from: accounts[0]});
+                }
+            });
+        });
+    });
+
+    describe('deleteFile', function () {
+        let fileName;
+        let storagePath;
+
+        beforeEach(async function () {
+            filestorage = await FileStorage.new({from: accounts[0]});
+            fileName = randomstring.generate();
+            let fileSize = Math.floor(Math.random()*100);
+            storagePath = rmBytesSymbol(accounts[0])+'/'+fileName;
+            await filestorage.startUpload(fileName, fileSize, {from: accounts[0]});
+        });
+
+        it('should delete file', async function () {
+            await filestorage.deleteFile(fileName, {from: accounts[0]});
+            let status = await filestorage.getFileStatus(storagePath);
+            assert.equal(status, 0);
+        });
+    });
 });
