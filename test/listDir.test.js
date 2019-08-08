2c8998910a4309d40f546664dfd21470ac34e08d
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2019-08-08 16:16:47 +0300

    SKALE-1342 Add listDir unit tests

--- test/listDir.test.js
+++ test/listDir.test.js
@@ -47,13 +47,15 @@ contract('Filestorage', accounts => {
             await filestorage.startUpload(path.join(dirName, fileName), 0, {from: accounts[0]});
             await filestorage.finishUpload(path.join(dirName, fileName), {from: accounts[0]});
             let content = await filestorage.listDir(dirPath);
-            assert.isArray(content);
-            assert.isArray(content.find(obj => {
+            let dirInfo = content.find(obj => {
                 return obj.name === dirName;
-            }));
-            assert.isArray(content.find(obj => {
+            });
+            let fileInfo = content.find(obj => {
                 return obj.name === fileName;
-            }));
+            });
+            assert.isArray(content);
+            assert.isTrue(parseInt(dirInfo.status) === 0 && dirInfo.isFile === false);
+            assert.isTrue(parseInt(fileInfo.status) === 2 && fileInfo.isFile === true);
         });
 
         it('should list files with chunks in directory', async function () {
@@ -64,16 +66,61 @@ contract('Filestorage', accounts => {
             }));
             let filePath = path.join(dirName, fileName);
             await filestorage.createDir(dirName, {from: accounts[0]});
+            await filestorage.startUpload(filePath, 2*CHUNK_LENGTH, {from: accounts[0]});
             await filestorage.uploadChunk(filePath, 0, data, {from: accounts[0], gas: UPLOADING_GAS});
             await filestorage.uploadChunk(filePath, CHUNK_LENGTH, data, {from: accounts[0], gas: UPLOADING_GAS});
-            await filestorage.startUpload(filePath, 3*CHUNK_LENGTH, {from: accounts[0]});
             await filestorage.finishUpload(filePath, {from: accounts[0]});
             let content = await filestorage.listDir(dirPath);
-            console.log(content);
             assert.isArray(content);
             assert.isArray(content.find(obj => {
                 return obj.name === fileName;
             }));
+        });
+
+        it('should list files with different statuses', async function () {
+            let fileName = randomstring.generate();
+            let unfinishedFileName = randomstring.generate();
+            let data = addBytesSymbol(randomstring.generate({
+                length: 2 * CHUNK_LENGTH,
+                charset: 'hex'
+            }));
+            let filePath = path.join(dirName, fileName);
+            let unfinishedFilePath = path.join(dirName, unfinishedFileName);
+            await filestorage.createDir(dirName, {from: accounts[0]});
+            await filestorage.startUpload(unfinishedFilePath, CHUNK_LENGTH, {from: accounts[0]});
+            await filestorage.startUpload(filePath, CHUNK_LENGTH, {from: accounts[0]});
+            await filestorage.uploadChunk(filePath, 0, data, {from: accounts[0], gas: UPLOADING_GAS});
+            await filestorage.finishUpload(filePath, {from: accounts[0]});
+            let content = await filestorage.listDir(dirPath);
+            let unfinishedInfo = content.find(obj => {
+                return obj.name === unfinishedFileName;
+            });
+            let finishedInfo = content.find(obj => {
+                return obj.name === fileName;
+            });
+            assert.isArray(finishedInfo);
+            assert.isTrue(finishedInfo.name === fileName &&
+                unfinishedInfo.name === unfinishedFileName, 'Incorrect fileName');
+            assert.isTrue(parseInt(finishedInfo.size) === CHUNK_LENGTH &&
+                parseInt(unfinishedInfo.size) === CHUNK_LENGTH, 'Incorrect fileSize');
+            assert.isTrue(parseInt(finishedInfo.status) === 2, 'Finished file: incorrect status');
+            assert.isTrue(finishedInfo.isChunkUploaded.length === 1, 'Finished file: incorrect chunk length');
+            assert.isTrue(finishedInfo.isChunkUploaded[0] === true, 'Finished file: incorrect chunks');
+            assert.isArray(unfinishedInfo);
+            assert.isTrue(parseInt(unfinishedInfo.status) === 1, 'Unfinished file: incorrect status');
+            assert.isTrue(unfinishedInfo.isChunkUploaded.length === 1, 'Unfinished file: incorrect chunk length');
+            assert.isTrue(unfinishedInfo.isChunkUploaded[0] === false, 'Unfinished file: incorrect chunks');
+        });
+
+        it('should list dirs in correct format', async function () {
+            await filestorage.createDir(dirName, {from: accounts[0]});
+            let content = await filestorage.listDir(rmBytesSymbol(accounts[0]) + '/');
+            let dirInfo = content.find(obj => {
+                return obj.name === dirName;
+            });
+            assert.isArray(dirInfo);
+            assert.equal(dirInfo.name, dirName);
+            assert.equal(dirInfo.isFile, false);
         });
 
         it('should list dirs and files in root directory', async function () {
