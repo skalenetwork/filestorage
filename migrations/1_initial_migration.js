f92de20924669e02d1e5b1f312454677f881dfcc
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2019-10-03 19:07:04 +0300

    SKALE-819 Update .travis.yml

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,11 +2,10 @@ const Migrations = artifacts.require("Migrations");
 const Filestorage = artifacts.require("FileStorage");
 const getFunds = require("../test/utils/getFunds");
 
-module.exports = function(deployer) {
-    // let currentNetwork = Object.keys( deployer.networks )[0];
-    // if (currentNetwork === 'skaled') {
-    //     await getFunds(deployer.networks[currentNetwork].from);
-    // }
+module.exports = async function(deployer) {
+    if (deployer.network === 'skaled') {
+        await getFunds(deployer.networks[deployer.network].from);
+    }
     deployer.deploy(Migrations);
     deployer.deploy(Filestorage);
 };
