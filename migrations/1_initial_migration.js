8c42dc38b039e14c4fd22337a30095899e85ab57
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2019-10-03 17:37:02 +0300

    SKALE-819 Skip unnecessary contracts

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,9 +2,11 @@ const Migrations = artifacts.require("Migrations");
 const Filestorage = artifacts.require("FileStorage");
 const getFunds = require("../test/utils/getFunds");
 
-module.exports = async function(deployer) {
-    let currentNetwork = Object.keys( deployer.networks )[0];
-    await getFunds(deployer.networks[currentNetwork].from);
+module.exports = function(deployer) {
+    // let currentNetwork = Object.keys( deployer.networks )[0];
+    // if (currentNetwork === 'skaled') {
+    //     await getFunds(deployer.networks[currentNetwork].from);
+    // }
     deployer.deploy(Migrations);
     deployer.deploy(Filestorage);
 };
