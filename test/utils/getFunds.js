9f8caaf782637dd330d6817f41431db2faa3e1b3
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2019-10-03 12:08:22 +0300

    SKALE-1517 Update tests for new version of skaled

---
+++ test/utils/getFunds.js
@@ -0,0 +1,39 @@
+require('dotenv').config({ path: '../../.env' });
+const Web3 = require('web3');
+const testBalance = '1';
+const rootPrivateKey = process.env.SCHAIN_OWNER_PK;
+const web3 = new Web3(process.env.ENTRYPOINT);
+console.log(process.env.ENTRYPOINT);
+async function getFunds(account) {
+    let testBalanceWei = await web3.utils.toWei(testBalance, 'ether');
+    let accountBalance = await web3.eth.getBalance(account);
+    let rootAccount = web3.eth.accounts.privateKeyToAccount(rootPrivateKey).address;
+    let rootBalance = await web3.eth.getBalance(rootAccount);
+    if (accountBalance < testBalanceWei) {
+        let valueToSend = testBalanceWei - accountBalance;
+        if (rootBalance < valueToSend) {
+            throw new Error('Insufficient funds for testing');
+        }
+        let tx = {
+            from: rootAccount,
+            gas: 21000,
+            to: account,
+            value: valueToSend
+        };
+        let signedTx = await web3.eth.accounts.signTransaction(tx, rootPrivateKey);
+        await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+        if (await web3.eth.getTransactionsCount(account) === 0) {
+            let tx = {
+                from: account,
+                gas: 21000,
+                to: account,
+                value: valueToSend
+            };
+            let signedTx = await web3.eth.accounts.signTransaction(tx, process.env.PRIVATEKEY);
+            await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+        }
+    }
+    return true;
+}
+
+module.exports = getFunds;
