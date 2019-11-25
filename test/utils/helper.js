ab57262a84803b57eddcf5867d2386a005b589be
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2019-11-25 12:53:20 +0200

    SKALE-1822 Get chainId

--- test/utils/helper.js
+++ test/utils/helper.js
@@ -18,7 +18,8 @@ async function getFunds(account) {
             from: rootAccount,
             gas: 21000,
             to: account,
-            value: valueToSend
+            value: valueToSend,
+            chainId: await web3.eth.getChainId()
         };
         let signedTx = await web3.eth.accounts.signTransaction(tx, rootPrivateKey);
         await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
@@ -39,7 +40,8 @@ async function sendTransaction(transactionData, to, gas, privateKey) {
         data: encoded,
         gas: gas,
         to: to,
-        nonce: nonce
+        nonce: nonce,
+        chainId: await web3.eth.getChainId()
     };
     let signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
     return await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
