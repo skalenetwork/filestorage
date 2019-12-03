5ffb713db48bbc83d3e6968e8e8ff0aa361eca79
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2019-12-03 13:07:37 +0200

    SKALE-1230 Automate config update

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,6 @@
 const PrivateKeyProvider = require("@truffle/hdwallet-provider");
+const path = require('path');
+const fs = require('fs');
 require('dotenv').config();
 
 module.exports = {
@@ -18,5 +20,14 @@ module.exports = {
 
   mocha: {
       enableTimeouts: false
+  },
+
+  build: function (options, callback) {
+      let configPath = path.join(options.destination_directory, 'contracts', 'FileStorage.json');
+      let skaledConfigPath = path.join(options.working_directory, 'test', 'utils', 'config.json');
+      let filestorageBytecode = require(configPath).deployedBytecode;
+      let skaledConfig = require(skaledConfigPath);
+      skaledConfig.accounts['0x69362535ec535F0643cBf62D16aDeDCAf32Ee6F7'].code = filestorageBytecode;
+      fs.writeFileSync(skaledConfigPath, JSON.stringify(skaledConfig, null, '\t'));
   }
 };
