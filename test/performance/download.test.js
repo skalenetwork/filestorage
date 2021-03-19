82975d0dde4cbba8bdee175ff1c4b0e2edf678e1
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2021-03-19 15:49:47 +0200

    SKALE-3726 Uncomment download tests

--- test/performance/download.test.js
+++ test/performance/download.test.js
@@ -32,44 +32,44 @@ contract('Filestorage', accounts => {
         return str.slice(2);
     }
 
-    // describe('Download performance test', async function () {
-    //     let fileName;
-    //     let storagePath;
-    //     let fileSize;
-    //
-    //     before(async function () {
-    //         filestorage = await initFilestorage(accounts[0], artifacts);
-    //         fileName = randomstring.generate();
-    //         storagePath = path.join(rmBytesSymbol(accounts[0]), fileName);
-    //         filePath = path.resolve(__dirname, "./image.jpg")
-    //         imgBuffer = fs.readFileSync(filePath);
-    //         imgHex = imgBuffer.toString('hex');
-    //         var stats = fs.statSync(filePath)
-    //         fileSize = stats.size;
-    //         let ptr = 0;
-    //         await filestorage.setChunkSize(CHUNK_LENGTH);
-    //         await filestorage.startUpload(fileName, fileSize, {from: accounts[0], gas: UPLOADING_GAS});
-    //         while(ptr < 2 * fileSize) {
-    //             finish = Math.min(ptr + 2 * CHUNK_LENGTH, 2 * fileSize + 1);
-    //             data = addBytesSymbol(imgHex.substring(ptr, finish))
-    //             await filestorage.uploadChunk(fileName, ptr / 2, data, {from: accounts[0], gas: UPLOADING_GAS});
-    //             ptr = finish;
-    //         }
-    //         await filestorage.finishUpload(fileName, {from: accounts[0]});
-    //     });
-    //
-    //     let iterationsCount = 5;
-    //     for (let i = 1; i <= iterationsCount; i++) {
-    //         it(`Attempt ${i} to read file`, async function () {
-    //             var i = 0;
-    //             while (i < fileSize) {
-    //                 let chunkLength = Math.min(CHUNK_LENGTH, fileSize - i);
-    //                 await filestorage.readChunk(storagePath, i, chunkLength, {gas: UPLOADING_GAS});
-    //                 i += chunkLength;
-    //             }
-    //         });
-    //     }
-    // });
+    describe('Download performance test', async function () {
+        let fileName;
+        let storagePath;
+        let fileSize;
+
+        before(async function () {
+            filestorage = await initFilestorage(accounts[0], artifacts);
+            fileName = randomstring.generate();
+            storagePath = path.join(rmBytesSymbol(accounts[0]), fileName);
+            filePath = path.resolve(__dirname, "./image.jpg")
+            imgBuffer = fs.readFileSync(filePath);
+            imgHex = imgBuffer.toString('hex');
+            var stats = fs.statSync(filePath)
+            fileSize = stats.size;
+            let ptr = 0;
+            await filestorage.setChunkSize(CHUNK_LENGTH);
+            await filestorage.startUpload(fileName, fileSize, {from: accounts[0], gas: UPLOADING_GAS});
+            while(ptr < 2 * fileSize) {
+                finish = Math.min(ptr + 2 * CHUNK_LENGTH, 2 * fileSize + 1);
+                data = addBytesSymbol(imgHex.substring(ptr, finish))
+                await filestorage.uploadChunk(fileName, ptr / 2, data, {from: accounts[0], gas: UPLOADING_GAS});
+                ptr = finish;
+            }
+            await filestorage.finishUpload(fileName, {from: accounts[0]});
+        });
+
+        let iterationsCount = 5;
+        for (let i = 1; i <= iterationsCount; i++) {
+            it(`Attempt ${i} to read file`, async function () {
+                var i = 0;
+                while (i < fileSize) {
+                    let chunkLength = Math.min(CHUNK_LENGTH, fileSize - i);
+                    await filestorage.readChunk(storagePath, i, chunkLength, {gas: UPLOADING_GAS});
+                    i += chunkLength;
+                }
+            });
+        }
+    });
 
     describe('Upload performance test', async function () {
         let fileSize;
