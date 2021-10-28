c72a1a8f5193c129912922efa6b49e116740a660
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2021-10-28 13:56:00 +0300

    SKALE-4592 Update gasLimit

--- truffle-config.js
+++ truffle-config.js
@@ -8,6 +8,7 @@ module.exports = {
       skaled: {
           gasPrice: 0,
           network_id: "*",
+          gas: 100000000,
           provider: () => new PrivateKeyProvider(process.env.SCHAIN_OWNER_PK, process.env.ENTRYPOINT)
       },
   },
