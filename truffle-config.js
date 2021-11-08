17c57d1d09d8e49fcf8f6b8b7ca44aa2449de716
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2021-11-08 17:55:42 +0200

    SKALE-4649 Add slither, add migrations.sol

--- truffle-config.js
+++ truffle-config.js
@@ -23,14 +23,5 @@ module.exports = {
       enableTimeouts: false
   },
 
-  plugins: ["solidity-coverage"],
-
-  build: function (options, callback) {
-      let configPath = path.join(options.destination_directory, 'contracts', 'FileStorage.json');
-      let skaledConfigPath = path.join(options.working_directory, 'test', 'utils', 'config.json');
-      let filestorageBytecode = require(configPath).deployedBytecode;
-      let skaledConfig = require(skaledConfigPath);
-      skaledConfig.accounts['0x69362535ec535F0643cBf62D16aDeDCAf32Ee6F7'].code = filestorageBytecode;
-      fs.writeFileSync(skaledConfigPath, JSON.stringify(skaledConfig, null, '\t'));
-  }
+  plugins: ["solidity-coverage"]
 };
