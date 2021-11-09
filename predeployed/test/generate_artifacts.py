13573d64b9607c6fd2066b7c9f2eb9a90596840f
Dmytro Nazarenko <dmytro.nazarenko@nure.ua>
2021-11-09 20:21:27 +0200

    SKALE-4649 Move artifacts generation to test/data

---
+++ predeployed/test/generate_artifacts.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+from src.filestorage_predeployed import FILESTORAGE_ADDRESS
+
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+CONTRACT_DATA_PATH = os.path.join(DIR_PATH, '..', 'src', 'filestorage_predeployed', 'artifacts', 'FileStorage.json')
+ARTIFACTS_PATH = os.path.join(DIR_PATH, 'data', 'artifacts.json')
+
+if __name__ == '__main__':
+    with open(CONTRACT_DATA_PATH, 'r') as f:
+        raw_contract_data = f.read()
+        contract_data = json.loads(raw_contract_data)
+    artifacts = {
+        'abi': contract_data['abi'],
+        'address': FILESTORAGE_ADDRESS
+    }
+    with open(ARTIFACTS_PATH, 'w') as f:
+        f.write(json.dumps(artifacts, indent=4))
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,12 @@
 var path = require('path');
 const fs = require('fs');
-const generatePredeployed = require('./generate');
+const contractData = require('../build/contracts/FileStorage.json');
 
 let artifactsPath = path.join(path.resolve(__dirname), '..', 'artifacts.json');
-let artifactsData;
-
-if (process.argv[2] && process.argv[2] === '--npm') {
-    artifactsData = generatePredeployed(true);
-} else {
-    artifactsData = generatePredeployed();
-}
+let FILESTORAGE_ADDRESS =
+let artifactsData = {
+    'abi': contractData.abi,
+    'address': FILESTORAGE_ADDRESS
+};
 
 fs.writeFileSync(artifactsPath, JSON.stringify(artifactsData, null, '\t'));
