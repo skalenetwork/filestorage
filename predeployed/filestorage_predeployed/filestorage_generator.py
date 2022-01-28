97e7b0b19b2574cc32bafc46b278a1e21fae92b6
Dmytro <dmitry.tkk@gmail.com>
2022-01-28 15:55:40 +0200

    Hotfix: pass abi param to init func

--- predeployed/filestorage_predeployed/filestorage_generator.py
+++ predeployed/filestorage_predeployed/filestorage_generator.py
@@ -45,7 +45,10 @@ class FileStorageGenerator(AccessControlEnumerableGenerator):
             dirname(__file__),
             'artifacts',
             self.ARTIFACT_FILENAME))
-        super().__init__(bytecode=generator.bytecode)
+        super().__init__(
+            bytecode=generator.bytecode,
+            abi=generator.abi
+        )
 
     @classmethod
     def generate_storage(cls, **kwargs) -> Dict[str, str]:
