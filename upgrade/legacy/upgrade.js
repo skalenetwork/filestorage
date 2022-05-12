fed4bc70792646a3027578e3a0252c27da04b052
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2022-05-12 15:46:15 +0300

    Add legacy upgrade script

---
+++ upgrade/legacy/upgrade.js
@@ -0,0 +1,50 @@
+const contractData = require('../build/contracts/FileStorage.json');
+const ozProxyAdmin = require('@openzeppelin/upgrades-core/artifacts/ProxyAdmin.json');
+const ozAdminUpgradeabilityProxy = require('@openzeppelin/upgrades-core/artifacts/AdminUpgradeabilityProxy.json');
+const Web3 = require('web3');
+const web3 = new Web3(process.env.ENTRYPOINT);
+
+const PROXY_ADMIN_ADDRESS = '0xD3001000000000000000000000000000000000D3';
+const FILESTORAGE_PROXY_ADDRESS = '0xD3002000000000000000000000000000000000D3';
+
+let pk = process.env.PRIVATE_KEY;
+let rootAccount = web3.eth.accounts.privateKeyToAccount(rootPrivateKey).address;
+
+async function deployImplementation() {
+    let implementationDeployment = new web3.eth.Contract(contractData.abi).deploy({
+        data: contractData.bytecode
+    });
+    let tx = {
+        from: rootAccount,
+        nonce: await web3.eth.getTransactionCount(rootAccount),
+        chainId: await web3.eth.getChainId(),
+        data: implementationDeployment.encodeABI(),
+    };
+    let gas = await implementationDeployment.estimateGas(tx);
+    tx.gas = gas;
+    let signedTx = await web3.eth.accounts.signTransaction(tx, pk);
+    await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+}
+
+async function switchImplementation(newAddress) {
+    let proxyAdmin = new web3.eth.Contract(ozProxyAdmin.abi, PROXY_ADMIN_ADDRESS);
+    let upgradeData = proxyAdmin.upgrade(FILESTORAGE_PROXY_ADDRESS, newAddress);
+    let tx = {
+        from: rootAccount,
+        to: PROXY_ADMIN_ADDRESS,
+        nonce: await web3.eth.getTransactionCount(rootAccount),
+        chainId: await web3.eth.getChainId(),
+        data: upgradeData.encodeABI(),
+    };
+    let gas = await upgradeData.estimateGas(tx);
+    tx.gas = gas;
+    let signedTx = await web3.eth.accounts.signTransaction(tx, pk);
+    await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+}
+
+async function upgrade() {
+    let newAddress = await deployImplementation();
+    await switchImplementation(newAddress);
+}
+
+upgrade();
