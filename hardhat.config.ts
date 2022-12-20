67a7cfed684740b7ee6351053a4ddf05e5994948
Vadim Yavorsky <yavrsky@gmail.com>
2022-12-20 13:09:12 +0200

    Add hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,50 @@
 import { HardhatUserConfig } from "hardhat/config";
+import "@openzeppelin/hardhat-upgrades";
+import "@nomiclabs/hardhat-ethers";
+import "@typechain/hardhat";
+
+function getCustomUrl(url: string | undefined) {
+  if (url) {
+    return url;
+  } else {
+    return "http://127.0.0.1:8545"
+  }
+}
+
+function getCustomPrivateKey(privateKey: string | undefined) {
+  if (privateKey) {
+    return [privateKey];
+  } else {
+    return [];
+  }
+}
+
+function getGasPrice(gasPrice: string | undefined) {
+  if (gasPrice) {
+    return parseInt(gasPrice, 10);
+  } else {
+    return "auto";
+  }
+}
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  defaultNetwork: "hardhat",
+  solidity: {
+    version: '0.8.11',
+    settings: {
+      optimizer:{
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    custom: {
+      url: getCustomUrl(process.env.ENDPOINT),
+      accounts: getCustomPrivateKey(process.env.PRIVATE_KEY),
+      gasPrice: getGasPrice(process.env.GASPRICE)
+    }
+  }
 };
 
 export default config;
